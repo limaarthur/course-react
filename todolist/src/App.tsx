522d42c14fd86ac369f446430fee731b7b47c0e2
limaarthur <artglima.dev@gmail.com>
2025-11-23 20:11:48 -0300

    creation button component

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -7,6 +7,7 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinerIcon from "./assets/icons/spiner.svg?react";
 import XIcon from "./assets/icons/x.svg?react";
 import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -43,6 +44,10 @@ export default function App() {
       <div>
         <Badge variant="secondary">5</Badge>
         <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
       </div>
     </div>
   )
