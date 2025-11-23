5a301c7166c32d2e73e986cf6d2ba808c1b9b523
limaarthur <artglima.dev@gmail.com>
2025-11-23 10:28:13 -0300

    creation icon component with cva and property animation

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,25 +1,43 @@
 import Text from "./components/text";
+import Icon from "./components/icon";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilSimpleIcon from "./assets/icons/pencil-simple.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinerIcon from "./assets/icons/spiner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text 
-      variant={"body-sm-bold"} 
-      className="text-pink-base">
-        Hello World
-      </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text 
+        variant={"body-sm-bold"} 
+        className="text-pink-base">
+          Hello World
+        </Text>
 
-      <Text 
-      variant={"body-md"} 
-      className="text-green-base">
-        Hello World
-      </Text>
+        <Text 
+        variant={"body-md"} 
+        className="text-green-base">
+          Hello World
+        </Text>
 
-      <Text 
-      variant={"body-md-bold"} 
-      className="text-pink-base">
-        Hello World
-      </Text>
+        <Text 
+        variant={"body-md-bold"} 
+        className="text-pink-base">
+          Hello World
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilSimpleIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinerIcon} animate />
+        <Icon svg={XIcon} className="fill-gray-400"/>
+      </div>
     </div>
   )
 }
