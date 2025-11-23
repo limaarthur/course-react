a781740ec83c55b29ad9fd31b1dca617f428e487
limaarthur <artglima.dev@gmail.com>
2025-11-22 23:48:56 -0300

    creation text component with cva

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,3 +1,25 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-pink-dark">Hello World</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text 
+      variant={"body-sm-bold"} 
+      className="text-pink-base">
+        Hello World
+      </Text>
+
+      <Text 
+      variant={"body-md"} 
+      className="text-green-base">
+        Hello World
+      </Text>
+
+      <Text 
+      variant={"body-md-bold"} 
+      className="text-pink-base">
+        Hello World
+      </Text>
+    </div>
+  )
 }
