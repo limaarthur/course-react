3eea35bc9ccab0589a608c0e24ade859840b5db0
limaarthur <artglima.dev@gmail.com>
2025-11-27 20:57:01 -0300

    creating main layout and rendering with outlet

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router"
 import PageComponents from "./pages/page-components"
+import LayoutMain from "./pages/layout-main"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/componentes" element={<PageComponents />} />
+        <Route element={<LayoutMain />}>
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
