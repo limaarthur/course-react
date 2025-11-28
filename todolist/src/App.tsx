cadfa5bdf9b7cd7efea952fd232d6c6d4d6a289c
limaarthur <artglima.dev@gmail.com>
2025-11-27 21:14:10 -0300

    home page creation and page transitions

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router"
 import PageComponents from "./pages/page-components"
 import LayoutMain from "./pages/layout-main"
+import PageHome from "./pages/page-home"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
           <Route path="/componentes" element={<PageComponents />} />
         </Route>
       </Routes>
