605dce8e0cea661d7060b5c26156c31bb4968884
limaarthur <artglima.dev@gmail.com>
2025-11-29 12:28:01 -0300

    updating task summary badge

--- todolist/src/core-components/tasks-summary.tsx
+++ todolist/src/core-components/tasks-summary.tsx
@@ -1,20 +1,22 @@
 import Text from "../components/text";
 import Badge from "../components/badge";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
+  const { createdTasksCount, concludedTasksCount } = useTasks();
   return (
     <>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">
           Tarefas criadas
         </Text>
-        <Badge variant="secondary">5</Badge>
+        <Badge variant="secondary">{createdTasksCount}</Badge>
       </div>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">
           Concluídas
         </Text>
-        <Badge variant="primary">2 de 5</Badge>
+        <Badge variant="primary">{concludedTasksCount} de {createdTasksCount}</Badge>
       </div>
     </>
   )
